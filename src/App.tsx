eee30d155269b2c1c81b0db1c276bb5839471b68
Dan Fusaro <danfusaro@rightpoint.com>
2022-06-17 16:22:14 -0400

    Added AWS message

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://aws.amazon.com/amplify/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn AWS Amplify
         </a>
       </header>
     </div>
