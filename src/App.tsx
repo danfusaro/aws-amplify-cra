39d3b246ff81b8b57b72605a582c43255fc6cc53
Dan Fusaro <danfusaro@rightpoint.com>
2022-06-21 22:57:27 -0400

    Added develop testing stuff

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn AWS Amplify
+          Learn AWS Amplify - This is Develop!
         </a>
       </header>
     </div>
